fa73cc4d21a0b5ad52fb5167b0d886204d25f37c
Kevin Simorangkir <mastercamp436@gmail.com>
2023-05-12 21:20:45 +0700

    new update

---
+++ main.py
@@ -0,0 +1,24 @@
+# Modul Utama
+import pygame
+import sys
+
+# Inisiasi PYGAME
+pygame.init()
+fps_clock = pygame.time.Clock()
+
+# Pengaturan
+SIZE = (1280, 720)
+icon = pygame.image.load("assets/images/icon.png")
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Lost")
+pygame.display.set_icon(icon)
+
+
+def main():
+    # game = Game(screen, fps_clock)
+    game.play()
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
