8005bc6fc4e4b1d9e37c0e7b004040a452a2fa6d
RizkiAlfaina <m.rizkialfaina@gmail.com>
2023-05-21 14:36:38 +0700

    update level_1 true

--- main.py
+++ main.py
@@ -63,10 +63,10 @@ asset.backsound.play(-1)
 volume_value = 0.4
 asset.backsound.set_volume(volume_value)
 
-level_1 = False
+level_1 = True
 level_2 = False
 level_3 = False
-level_4 = True
+level_4 = False
 level_5 = False
 level_6 = False
 
