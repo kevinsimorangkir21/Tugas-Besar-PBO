8405d8593017d112ee49888ccb4b1ab4b631c8a8
RizkiAlfaina <m.rizkialfaina@gmail.com>
2023-05-26 20:40:28 +0700

    Update asset.py

--- asset.py
+++ asset.py
@@ -7,7 +7,7 @@ class Game:
         self.current_fighter = 1
         self.total_fighters_on_3_enemy = 4
         self.total_fighters_on_2_enemy = 3
-        self.total_fighters_on_1_enemy = 2
+        self.total_fighters_on_1_enemy = 2 
         self.action_cooldown = 0
         self.action_wait_time = 90
         self.attack = False
